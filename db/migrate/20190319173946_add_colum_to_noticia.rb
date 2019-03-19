5d9397b96a6a277230889abd80781f3c8853a585
erniux <erna.tercero@hotmail.com>
2019-03-19 16:24:06 -0600

    eliminacion de migrations de tablas noticias

--- db/migrate/20190319173946_add_colum_to_noticia.rb
+++
@@ -1,5 +0,0 @@
-class AddColumToNoticia < ActiveRecord::Migration[5.2]
-  def change
-    add_column :noticia, :imagen, :string
-  end
-end
--- db/migrate/20190319174946_drop_table_noticia.rb
+++
@@ -1,5 +0,0 @@
-class DropTableNoticia < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :noticia
-  end
-end
