6c8416f565cda10041c7d6ea10d428898c12f2a1
erniux <erna.tercero@hotmail.com>
2019-03-29 06:21:23 -0600

    creando modulo de Administracion de aplicacion

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
       root to: "users#index"
     end
+    
   resources :avisos
   devise_for :users, path: '',path_names: {sign_in: 'ingresar', sign_out: 'salir', sign_up: 'registro'}
   resources :eventos
