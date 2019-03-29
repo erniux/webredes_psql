eacaef08468608b36eee2c2f8bfe27a09144049d
erniux <erna.tercero@hotmail.com>
2019-03-29 14:46:59 -0600

    actualizacion de mensajes de autorizacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
    include DeviseWhitelist
    include Pundit
 
+   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:notice] = "Usted no esta autorizado para ejecutar esta función."
+    redirect_to(root_path)
+  end
+
 end
