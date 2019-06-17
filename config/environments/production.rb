9fc479fe441bacc1713e35fafbb75f3c6a4f21fa
erniux <erna.tercero@hotmail.com>
2019-06-17 11:32:30 -0500

    purebas correo productivo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
+   
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  
+   config.action_mailer.delivery_method = :smtp
  
    config.action_mailer.smtp_settings = {
      address: "smtp.gmail.com",
@@ -102,7 +108,7 @@ Rails.application.configure do
      domain: "gmail.com",
      authentication: "plain",
      enable_starttls_auto: true,
-     user_name: "info@EscuelasSeguras.red",
-     password: "vovfeb-vahfog-safFi3"
+     user_name: "desarrolloswat@gmail.com",
+     password: "fevza0-gybdef-cAjniq"
    }
 end
