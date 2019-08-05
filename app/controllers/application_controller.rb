9381c4217dd64866d00cf32e92cc0e32d08cc192
erniux <erna.tercero@hotmail.com>
2019-08-04 23:54:43 -0500

    cambios y mejoras agosto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery 
 
-	before_action :authenticate_user!
+	#before_action :authenticate_user!
 	
    include DeviseWhitelist
    include AliadaCopyright
