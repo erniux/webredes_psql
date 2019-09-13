f99c3627e9868e728813156dc7c84e670f0aea34
erniux <erna.tercero@hotmail.com>
2019-09-13 11:56:07 -0500

    validaciones heroku sockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,8 @@ Rails.application.configure do
   
    config.action_mailer.delivery_method = :smtp
  
+ config.web_socket_server_url = "wss://webredes.herokuapp.com/cable" 
+
    config.action_mailer.smtp_settings = {
      address: "smtp.gmail.com",
      port: 587,
