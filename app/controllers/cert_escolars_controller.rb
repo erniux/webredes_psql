263dfbac695374faf067065d90f3fb981eb95d29
erniux <erna.tercero@hotmail.com>
2020-01-02 17:40:24 -0600

    validacion de collapse en heroku 1.3

--- app/controllers/cert_escolars_controller.rb
+++ app/controllers/cert_escolars_controller.rb
@@ -8,7 +8,6 @@ class CertEscolarsController < ApplicationController
         @results = CertEscolar.search_by_full_escuela(params[:term]).order(proceso: 'ASC', paso: 'ASC', estandar: 'ASC')
       else
         @results = CertEscolar.all.order(escuela_id: 'ASC', proceso: 'ASC', paso: 'ASC', estandar: 'ASC')
-        puts @results.inspect
       end
     elsif current_user.has_role?(:escuela)
       @results = CertEscolar.where(escuela_id: current_user.escuela_id).all.order(proceso: 'ASC', paso: 'ASC', estandar: 'ASC')
